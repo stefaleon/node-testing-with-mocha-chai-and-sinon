9e0348b1a8e23bfd25e36a7fe9e46a7aee4b132f
stefaleon <stefaleon@gmail.com>
2020-04-15 15:48:02 +0300

    using stubs

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -1,24 +1,50 @@
 const expect = require('chai').expect;
+const jwt = require('jsonwebtoken');
+const sinon = require('sinon');
 
 const authMiddleware = require('../middleware/is-auth');
 
-describe('Auth middleware', function() {
-  it('should throw an error if no authorization header is present', function() {
+describe('Auth middleware', function () {
+  it('should throw an error if no authorization header is present', function () {
     const req = {
-      get: function(headerName) {
+      get: function (headerName) {
         return null;
-      }
+      },
     };
     expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
       'Not authenticated.'
     );
   });
 
-  it('should throw an error if the authorization header is only one string', function() {
+  it('should throw an error if the authorization header is only one string', function () {
     const req = {
-      get: function(headerName) {
+      get: function (headerName) {
         return 'xyz';
-      }
+      },
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
+
+  it('should yield a userId after decoding the token', function () {
+    const req = {
+      get: function (headerName) {
+        return 'Bearer djfkalsdjfaslfjdlas';
+      },
+    };
+    sinon.stub(jwt, 'verify'); // replaces the 'verify' method of the 'jwt' object with a stub function.
+    jwt.verify.returns({ userId: 'abc' }); // configure the stub method to return what we need for our testing
+    authMiddleware(req, {}, () => {}); // now call the subject of our testing
+    expect(req).to.have.property('userId');
+    expect(req).to.have.property('userId', 'abc');
+    expect(jwt.verify.called).to.be.true;
+    jwt.verify.restore(); // restore the original method in order to have it available for other tests
+  });
+
+  it('should throw an error if the token cannot be verified', function () {
+    const req = {
+      get: function (headerName) {
+        return 'Bearer xyz';
+      },
     };
     expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
   });
